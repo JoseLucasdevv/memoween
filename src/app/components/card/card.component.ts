f5d3e86992dca4c5bf9335e19a1160fd86d7c580
jose-lucas <joselucas.dev@gmail.com>
2024-08-20 10:08:22 -0300

    feat:add card component finished

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,42 @@
+import { Component,Input, ViewChild,ElementRef } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss',
+})
+export class CardComponent {
+  cardFlipElement!:HTMLElement
+  @Input() imageSrc!: String;
+  @Input() imageId!: Number;
+
+  @ViewChild('cardFlip') cardFlip!: ElementRef;
+
+  rotationDeg: Number = 0;
+
+
+  ngAfterViewInit(): void {
+    this.cardFlipElement= this.cardFlip.nativeElement;
+
+  }
+
+
+async flipCardWhenStartForSeconds(){
+
+await new Promise<void>((resolve) => {
+  this.cardFlipElement.classList.add('control-flip');
+    setTimeout(() => {
+      resolve();
+    },2000);
+  });
+
+
+  this.cardFlipElement.classList.remove('control-flip');
+
+    }
+
+}
